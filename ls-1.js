e39ea0feddc0c6f7e2ec1bb6d75809bcd2282502
Nitesh Shetye <niteshshetye204@gmail.com>
2025-09-16 21:44:47 +0530

    first commit

---
+++ ls-1.js
@@ -0,0 +1,26 @@
+// LOOPS COVERD
+for (let i = 0; i < 5; i++) {
+  console.log("Hello world ", i);
+}
+console.log("****************");
+
+// REVERSE LOOP
+for (let i = 5; i > 0; i--) {
+  console.log("Hello world ", i);
+}
+console.log("****************");
+
+// LOOP THROUGH ARRAY
+const arr = [1, 2, 3, 4, 5];
+for (let i = 0; i < arr.length; i++) {
+  console.log("Array element ", arr[i]);
+}
+console.log("****************");
+
+// WHILE LOOP
+let j = 0;
+while (j < 5) {
+  console.log("Hello world ", j);
+  j++;
+}
+console.log("****************");
