a83a0ed0d127178b6ea6e10b817a73be652bff52
Nitesh Shetye <niteshshetye204@gmail.com>
2025-09-16 22:02:42 +0530

    LS-3 second largest number done

---
+++ ls-3.js
@@ -0,0 +1,22 @@
+// Second Largest Element in an Array
+
+const arr = [8, 5, 7, 0, 9, 0, 4];
+
+function secondLargestNumber(arr) {
+  let largestNumber = -Infinity;
+  let secondLargestNumber = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largestNumber) {
+      secondLargestNumber = largestNumber;
+      largestNumber = arr[i];
+    } else if (arr[i] > secondLargestNumber && arr[i] != largestNumber) {
+      secondLargestNumber = arr[i];
+    }
+  }
+
+  return secondLargestNumber;
+}
+
+const value = secondLargestNumber(arr);
+console.log(value);
